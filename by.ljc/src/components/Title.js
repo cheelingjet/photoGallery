7a70e918564561bcc24182802c568ff3b9058e46
cheelingjet <lchee@purdue.edu>
2024-02-19 21:40:43 -0500

    Font styling changes

--- by.ljc/src/components/Title.js
+++ by.ljc/src/components/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>by.ljc</h1>
+      <h1 className='logo-font'>by.ljc</h1>
       <h2>through my lens</h2>
-      <p>photography is a passion of mine, take a view into my life and enjoy!</p>
+      <p className='p-font'>capture the moments that matter most and your memory of the good times might just last longer</p>
     </div>
   )
 }
